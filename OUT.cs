7fa77af8e42e66fb822dd9551adfcc50f01aff05
olimov Amir <olimov.amir@mail.ru>
2023-12-03 09:31:11 +0300

    Добавьте файлы проекта.

---
+++ OUT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OUT
+{
+    class OUT
+    {
+        static void Foo(ref int value)
+        {
+            Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
