14ed53943bb06eb8e0562cfd050500e8893786a3
olimov Amir <olimov.amir@mail.ru>
2023-12-03 09:42:21 +0300

    v1

--- OUT.cs
+++ OUT.cs
@@ -10,10 +10,23 @@ namespace OUT
     {
         static void Foo(ref int value)
         {
+            value++;
+            Console.WriteLine(value);
+        }
+
+        static void Bar(out int value)
+        {
+            value = 5;
             Console.WriteLine(value);
         }
         static void Main(string[] args)
         {
+            int a = 10;
+
+            int b = 20;
+
+            Foo(ref a);
+            Bar(out b);
 
         }
     }
